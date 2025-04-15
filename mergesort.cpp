add4f9d47d1d65f948cc38b9136a3766c80d1bdb
mdeandwibekti <mdeandwibekti@gmail.com>
2025-04-15 11:21:16 +0700

    Ulangi sampai i > tengah: // Jika masih ada beberapa elemen di // sublist pertama tambahkan ke daftar baru

--- mergesort.cpp
+++ mergesort.cpp
@@ -65,4 +65,16 @@ void mergeSort(int low, int high)
         j++;
         k = k + 1;
     }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+    
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
 }
