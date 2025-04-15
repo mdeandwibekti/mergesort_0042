3e97b3a8a57ffdfc61b311e2b37b6cc6c1351ba5
mdeandwibekti <mdeandwibekti@gmail.com>
2025-04-15 11:06:13 +0700

    membuat panjang elemen dan maksimal elemen

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,3 +3,21 @@ using namespace std;
  
 int arr[20], B[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukan panjang element array:";
+        cin >> n;
+
+        if (n <= 20)
+        {
+        break;
+        }
+        else{
+            cout << "\nmaksimal panjnag array adalah 20";
+        }
+    }
+    
+}
